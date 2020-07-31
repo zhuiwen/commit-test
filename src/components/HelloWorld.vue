a68153bb4f9339e3c6b6a4dd451a0697320a9de7
zhuiwen <1227287697@qq.com>
2020-07-31 16:58:40 +0800

    测试提交项目

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="hello">
+    
+    <ul>
+      <li v-for="(item,index) in list" :key="item.id"> <span>{{item.id}}</span> <input type="checkbox" ><button @click="del(index)">del</button>  </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+// Diff
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      list:[{
+        id:1,
+        text:"a"
+      },{
+        id:1,
+        text:"b"
+      },{
+        id:3,
+        text:"c"
+      }]
+    }
+  },
+  methods:{
+    del(i){
+      this.list.splice(i,1);
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
